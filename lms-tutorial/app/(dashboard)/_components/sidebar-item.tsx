a5ae568c19a68c4f734fbfcf6245a3e115409d94
alexSiera <fa1nt.siera@gmail.com>
2024-03-05 00:16:22 +0300

    1:00:00 Categories Query

---
+++ lms-tutorial/app/(dashboard)/_components/sidebar-item.tsx
@@ -0,0 +1,13 @@
+"use cilent";
+
+import { LucideIcon } from "lucide-react";
+
+interface SidebarItemProps {
+  icon: LucideIcon;
+  label: string;
+  href: string;
+}
+
+export const SidebarItem = ({ icon, label, href }: SidebarItemProps) => {
+  return <div>Sidebar Item!</div>;
+};
