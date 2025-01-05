9cd92480bb9e1b2b5c0f333ae3210f88bfd0f22a
out-pavlovtsev.a.a <out-pavlovtsev.a.a@sbertech.ru>
2025-01-06 02:30:24 +0300

    chat-pdf: 19:23

---
+++ chat-pdf/src/app/page.tsx
@@ -0,0 +1,9 @@
+import MaxWidthWrapper from "./components/MaxWidthWrapper";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper className="bg-red-500">
+      <div>Helllo world</div>
+    </MaxWidthWrapper>
+  );
+}
