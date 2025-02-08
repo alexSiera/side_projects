a76c96e1f1a6cf1b6fbb08efab28872fde12fdaf
out-pavlovtsev.a.a <out-pavlovtsev.a.a@sbertech.ru>
2025-02-09 02:23:38 +0300

    3:37:00 Designing our Product Page

--- chat-pdf/src/app/dashboard/[fileId]/page.tsx
+++ chat-pdf/src/app/dashboard/[fileId]/page.tsx
@@ -1,14 +1,47 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
 interface PageProps {
-  params: {
-    fileId: string;
-  };
+  params: Promise<{
+    fileid: string;
+  }>;
 }
 
-const Page = ({ params }: PageProps) => {
+const Page = async ({ params }: PageProps) => {
+  const { fileid } = await params;
   // retrieve the file id
-  const { fileId } = params;
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
+  if (!user || !user.id) {
+    redirect(`/auth-callback?origin=dashboard/${fileid}`);
+  }
+
   // make db call
-  <div>{fileId}</div>;
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
+
+  if (!file) {
+    notFound();
+  }
+
+  return (
+    <div className='h-[calc(100vh - 3.5rem)] flex flex-1 flex-col justify-between'>
+      <div className='max-w-8xl mx-auto w-full grow lg:flex xl:px-2'>
+        {/* left side */}
+        <div className='flex-1 xl:flex'>
+          <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
