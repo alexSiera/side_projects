1a9ee1fc25f2b1df0da7a9c2de9d847aed479346
out-pavlovtsev.a.a <out-pavlovtsev.a.a@sbertech.ru>
2025-02-09 00:41:47 +0300

    3:09:15 Perfecting our dashboard

--- chat-pdf/src/components/Dashboard.tsx
+++ chat-pdf/src/components/Dashboard.tsx
@@ -2,8 +2,11 @@
 
 import { trpc } from "@/app/_trpc/client";
 import UploadButton from "./UploadButton";
-import { Ghost } from "lucide-react";
+import { Ghost, MessageSquare, Plus, Trash } from "lucide-react";
 import Skeleton from "react-loading-skeleton";
+import Link from "next/link";
+import dayjs from "dayjs";
+import { Button } from "./ui/button";
 
 const Dashboard = () => {
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
@@ -28,7 +31,36 @@ const Dashboard = () => {
                 key={file.id}
                 className='col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg'
               >
-                name
+                <Link
+                  href={`/dashboard/${file.id}`}
+                  className='flex flex-col gap-2'
+                >
+                  <div className='flex w-full items-center justify-between space-x-6 px-6 pt-6'>
+                    <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500'>
+                      {" "}
+                    </div>
+                    <div className='flex-1 truncate'>
+                      <div className='flex items-center space-x-3'>
+                        <h3 className='truncate text-lg font-medium text-zinc-900'>
+                          {file.name}
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+                <div className='mt-4 grid grid-cols-3 place-items-center gap-6 px-6 py-2 text-xs text-zinc-500'>
+                  <div className='flex items-center gap-2'>
+                    <Plus className='h-4 w-4' />
+                    {dayjs(file.createdAt).format("MMM YYYY")}
+                  </div>
+                  <div className='flex items-center gap-2'>
+                    <MessageSquare className='h-4 w-4' />
+                    mocked
+                  </div>
+                  <Button size='sm' className='w-full' variant='destructive'>
+                    <Trash className='h-4 w-4' />
+                  </Button>
+                </div>
               </li>
             ))}
         </ul>
