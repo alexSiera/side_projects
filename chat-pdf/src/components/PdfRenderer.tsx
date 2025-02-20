8400fb19e468a1a00790a7192639f04a09196dcd
out-pavlovtsev.a.a <out-pavlovtsev.a.a@sbertech.ru>
2025-02-20 10:58:36 +0300

    5:06:00 Rendering PDF files

--- chat-pdf/src/components/PdfRenderer.tsx
+++ chat-pdf/src/components/PdfRenderer.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import { Loader2 } from "lucide-react";
+import { useToast } from "@/hooks/use-toast";
+import { ChevronDown, Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
+import { useResizeDetector } from "react-resize-detector";
+
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
+import { Button } from "./ui/button";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -15,23 +19,37 @@ interface Props {
 }
 
 const PdfRenderer = ({ url }: Props) => {
+  const { toast } = useToast();
+  const { width, ref } = useResizeDetector();
+
   return (
     <div className='flex w-full flex-col items-center rounded-md bg-white shadow'>
       <div className='flex h-14 w-full items-center justify-between border-b border-zinc-200 px-2'>
-        <div className='flex items-center gap-1.5'>top bar</div>
+        <div className='flex items-center gap-1.5'>
+          <Button variant='ghost' aria-label='previous page'>
+            <ChevronDown className='h-4 w-4' />
+          </Button>
+        </div>
       </div>
       <div className='max-h-screen w-full flex-1'>
-        <div>
+        <div ref={ref}>
           <Document
             loading={
               <div className='flex justify-center'>
                 <Loader2 className='my-24 h-6 w-6 animate-spin' />
               </div>
             }
+            onLoadError={() => {
+              toast({
+                title: "Error loading pdf",
+                description: "Please try again later",
+                variant: "destructive",
+              });
+            }}
             file={url}
             className='max-h-full'
           >
-            <Page pageNumber={1} />
+            <Page width={width ? width : 1} pageNumber={1} />
           </Document>
         </div>
       </div>
